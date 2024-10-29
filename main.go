6505068ae0183e9c8b9413aaf12171cc8c10d6ec
“firstname <dawitsarsenbaev@gmail.com>
2024-10-29 20:59:41 +0500

    Add slice iteration in transaction

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ func main() {
 		}
 		transactions = append(transactions, transaction)
 	}
+	transactionSum := calculateBalance(transactions)
 	fmt.Println(transactions)
+	fmt.Printf("Your balance: %.2f", transactionSum)
 }
 
 func scanTransaction() float64 {
@@ -19,4 +21,12 @@ func scanTransaction() float64 {
 	fmt.Print("Enter the transaction (n for exit): ")
 	fmt.Scan(&transaction)
 	return transaction
+}
+
+func calculateBalance(transactions []float64) float64 {
+	var transactionSum float64 = 0.0
+	for _, value := range transactions {
+		transactionSum = transactionSum + value 
+	}
+	return transactionSum
 }
